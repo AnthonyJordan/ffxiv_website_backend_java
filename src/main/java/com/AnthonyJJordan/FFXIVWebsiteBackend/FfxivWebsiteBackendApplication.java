9d405eb9b82de8d1aca014c928dbda128f72a507
Anthony <Kain235@gmail.com>
2022-08-26 13:15:40 -0400

    Initial commit

---
+++ src/main/java/com/AnthonyJJordan/FFXIVWebsiteBackend/FfxivWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.AnthonyJJordan.FFXIVWebsiteBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfxivWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfxivWebsiteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AnthonyJJordan/FFXIVWebsiteBackend/FfxivWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AnthonyJJordan.FFXIVWebsiteBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfxivWebsiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
